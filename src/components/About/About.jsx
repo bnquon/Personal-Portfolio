58426b4ee5cc933c8d17536855726ede4c614cd1
Brandon Quon <bqa7@sfu.ca>
2024-03-27 22:01:29 -0700

    Changed to intersection observer

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { useParallax } from "react-scroll-parallax";
+import { useInView } from 'react-intersection-observer'
 import './About.css'
 import pfp from '../../assets/images/icon.jpg'
 
 export const About = () => {
 
-  const aboutParallax = useParallax({
-    translateY: [0, -200],
+  const [bioView, inView] = useInView({
+    threshold: 0,
   })
 
   return (
@@ -19,8 +19,8 @@ export const About = () => {
             </div>
 
             <div id="bio">
-                <h1 ref={aboutParallax.ref}>ABOUT ME</h1>
-                <span>
+                <h1 style={{transform: inView ? 'translateY(-100%)': 'translateY(0)'}}>ABOUT ME</h1>
+                <span ref={bioView}>
                     Hello there! I'm Brandon Quon, a second-year student at Simon Fraser University studying computer science. 
                     I'm on an exciting journey fueled by my passion for lifelong learning and my insatiable curiosity about all things related to 
                     technology. As I continue to learn and grow, I look forward to sharing more exciting projects and improvements on this site.
